36431a967fe7d8acdd32e23c48465b5d37f32c3e
yangxuan <xuan.yang@zilliz.com>
2024-04-10 17:40:50 +0800

    enhance: Tidy compactor and remove dup codes
    Add compaction package
    Add more ut for merge
    
    Signed-off-by: yangxuan <xuan.yang@zilliz.com>
    
    add more tests
    
    Signed-off-by: yangxuan <xuan.yang@zilliz.com>
    
    remove compactor.go and compactor_test.go
    
    Signed-off-by: yangxuan <xuan.yang@zilliz.com>

--- internal/datanode/writebuffer/insert_buffer.go
+++ internal/datanode/writebuffer/insert_buffer.go
@@ -1,6 +1,7 @@
 package writebuffer
 
 import (
+	"fmt"
 	"math"
 
 	"github.com/cockroachdb/errors"
@@ -8,6 +9,7 @@ import (
 
 	"github.com/milvus-io/milvus-proto/go-api/v2/msgpb"
 	"github.com/milvus-io/milvus-proto/go-api/v2/schemapb"
+	"github.com/milvus-io/milvus/internal/proto/etcdpb"
 	"github.com/milvus-io/milvus/internal/storage"
 	"github.com/milvus-io/milvus/pkg/log"
 	"github.com/milvus-io/milvus/pkg/util/paramtable"
@@ -80,6 +82,95 @@ type InsertBuffer struct {
 	buffer *storage.InsertData
 }
 
+// SegmentInsertBuffer can be reused to buffer all insert data of one segment
+// buffer.Serialize will serialize the InsertBuffer and clear it
+// pkstats keeps tracking pkstats of the segment until Finish
+type SegmentInsertBuffer struct {
+	*InsertBuffer
+
+	pkstats      *storage.PrimaryKeyStats
+	segmentID    int64
+	partitionID  int64
+	collectionID int64
+}
+
+func NewSegmentInsertBuffer(sch *schemapb.CollectionSchema, maxCount int64, segID, partID, collID int64) (*SegmentInsertBuffer, error) {
+	ibuffer, err := NewInsertBuffer(sch)
+	if err != nil {
+		return nil, err
+	}
+
+	var pkField *schemapb.FieldSchema
+	for _, fs := range sch.GetFields() {
+		if fs.GetIsPrimaryKey() && fs.GetFieldID() >= 100 && typeutil.IsPrimaryFieldType(fs.GetDataType()) {
+			pkField = fs
+		}
+	}
+	if pkField == nil {
+		log.Warn("failed to get pk field from schema")
+		return nil, fmt.Errorf("no pk field in schema")
+	}
+
+	stats, err := storage.NewPrimaryKeyStats(pkField.GetFieldID(), int64(pkField.GetDataType()), maxCount)
+	if err != nil {
+		return nil, err
+	}
+
+	return &SegmentInsertBuffer{
+		InsertBuffer: ibuffer,
+		pkstats:      stats,
+		segmentID:    segID,
+		partitionID:  partID,
+		collectionID: collID,
+	}, nil
+}
+
+func (b *SegmentInsertBuffer) Clear() {
+	ibuffer, _ := NewInsertBuffer(b.collSchema)
+	b.InsertBuffer = ibuffer
+}
+
+func (b *SegmentInsertBuffer) GetRowNum() int64 {
+	return int64(b.rows)
+}
+
+func (b *SegmentInsertBuffer) GetCollectionID() int64 {
+	return b.collectionID
+}
+
+func (b *SegmentInsertBuffer) GetPartitionID() int64 {
+	return b.partitionID
+}
+
+func (b *SegmentInsertBuffer) GetSegmentID() int64 {
+	return b.segmentID
+}
+
+func (b *SegmentInsertBuffer) GetPkID() int64 {
+	return b.pkstats.FieldID
+}
+
+// Serialize the current InsertBuffer
+func (b *SegmentInsertBuffer) SerializeYield() ([]*storage.Blob, *TimeRange, error) {
+	codec := storage.NewInsertCodecWithSchema(&etcdpb.CollectionMeta{ID: b.collectionID, Schema: b.collSchema})
+	blobs, err := codec.Serialize(b.partitionID, b.segmentID, b.Yield())
+	tr := b.GetTimeRange()
+	b.Clear()
+
+	return blobs, tr, err
+}
+
+// End the life cycle of this buffer and return the stats blobs
+func (b *SegmentInsertBuffer) Finish(actualRowCount int64) (*storage.Blob, error) {
+	codec := storage.NewInsertCodecWithSchema(&etcdpb.CollectionMeta{ID: b.collectionID, Schema: b.collSchema})
+	return codec.SerializePkStats(b.pkstats, actualRowCount)
+}
+
+func (b *SegmentInsertBuffer) BufferRow(row map[int64]interface{}, pk storage.PrimaryKey, ts uint64) error {
+	b.pkstats.Update(pk)
+	return b.InsertBuffer.BufferRow(row, ts)
+}
+
 func NewInsertBuffer(sch *schemapb.CollectionSchema) (*InsertBuffer, error) {
 	estSize, err := typeutil.EstimateSizePerRecord(sch)
 	if err != nil {
@@ -116,17 +207,35 @@ func (ib *InsertBuffer) Yield() *storage.InsertData {
 	return ib.buffer
 }
 
+func (ib *InsertBuffer) BufferRow(row map[int64]interface{}, ts uint64) error {
+	if err := ib.buffer.Append(row); err != nil {
+		return err
+	}
+
+	ib.rows += 1
+	ib.size += int64(ib.buffer.GetRowSize(ib.buffer.GetRowNum() - 1))
+
+	if ts < ib.TimestampFrom {
+		ib.TimestampFrom = ts
+	}
+	if ts > ib.TimestampTo {
+		ib.TimestampTo = ts
+	}
+
+	return nil
+}
+
 func (ib *InsertBuffer) Buffer(inData *inData, startPos, endPos *msgpb.MsgPosition) int64 {
-	totalMemSize := int64(0)
+	bufferedSize := int64(0)
 	for idx, data := range inData.data {
 		storage.MergeInsertData(ib.buffer, data)
 		tsData := inData.tsField[idx]
 
 		// update buffer size
 		ib.UpdateStatistics(int64(data.GetRowNum()), int64(data.GetMemorySize()), ib.getTimestampRange(tsData), startPos, endPos)
-		totalMemSize += int64(data.GetMemorySize())
+		bufferedSize += int64(data.GetMemorySize())
 	}
-	return totalMemSize
+	return bufferedSize
 }
 
 func (ib *InsertBuffer) getTimestampRange(tsData *storage.Int64FieldData) TimeRange {
