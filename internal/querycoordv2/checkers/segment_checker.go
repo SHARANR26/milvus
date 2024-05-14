a8437a19a366581e0a58bf9e9505b7545c701637
Wei Liu <wei.liu@zilliz.com>
2024-05-14 11:35:26 +0800

    enhance: Enable to dynamic update balancer policy in querycoord
    This PR enable to dynamic update balancer policy without restart
    querycoord.
    
    Signed-off-by: Wei Liu <wei.liu@zilliz.com>

--- internal/querycoordv2/checkers/segment_checker.go
+++ internal/querycoordv2/checkers/segment_checker.go
@@ -41,27 +41,27 @@ const initialTargetVersion = int64(0)
 
 type SegmentChecker struct {
 	*checkerActivation
-	meta      *meta.Meta
-	dist      *meta.DistributionManager
-	targetMgr *meta.TargetManager
-	balancer  balance.Balance
-	nodeMgr   *session.NodeManager
+	meta            *meta.Meta
+	dist            *meta.DistributionManager
+	targetMgr       *meta.TargetManager
+	nodeMgr         *session.NodeManager
+	getBalancerFunc GetBalancerFunc
 }
 
 func NewSegmentChecker(
 	meta *meta.Meta,
 	dist *meta.DistributionManager,
 	targetMgr *meta.TargetManager,
-	balancer balance.Balance,
 	nodeMgr *session.NodeManager,
+	getBalancerFunc GetBalancerFunc,
 ) *SegmentChecker {
 	return &SegmentChecker{
 		checkerActivation: newCheckerActivation(),
 		meta:              meta,
 		dist:              dist,
 		targetMgr:         targetMgr,
-		balancer:          balancer,
 		nodeMgr:           nodeMgr,
+		getBalancerFunc:   getBalancerFunc,
 	}
 }
 
@@ -424,7 +424,7 @@ func (c *SegmentChecker) createSegmentLoadTasks(ctx context.Context, segments []
 				SegmentInfo: s,
 			}
 		})
-		shardPlans := c.balancer.AssignSegment(replica.GetCollectionID(), segmentInfos, availableNodes, false)
+		shardPlans := c.getBalancerFunc().AssignSegment(replica.GetCollectionID(), segmentInfos, availableNodes, false)
 		for i := range shardPlans {
 			shardPlans[i].Replica = replica
 		}
